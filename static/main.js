9ce792b7b36a647260cd34ba07a210a238507084
Daniel Schep <dschep@gmail.com>
2018-01-15 18:41:21 -0500

    lint

--- static/main.js
+++ static/main.js
@@ -1,81 +1,81 @@
-API_ROOT = 'https://g4k8bjzsp8.execute-api.us-east-1.amazonaws.com/dev'
+const API_ROOT = 'https://g4k8bjzsp8.execute-api.us-east-1.amazonaws.com/dev';
 
 const map = L.map('map').setView([38.918, -77.040], 11);
 let userLocation;
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
 const AddStandControl = L.Control.extend({
-    options: {
-        position: 'topright'
-    },
+  options: {
+    position: 'topright'
+  },
 
-    onAdd: function (map) {
-        // create the control container with a particular class name
-        const container = L.DomUtil.create('div', 'leaflet-bar add-control');
+  onAdd: function (map) {
+    // create the control container with a particular class name
+    const container = L.DomUtil.create('div', 'leaflet-bar add-control');
 
-        container.innerHTML = '<a href="#"><i class="fa fa-plus-square-o fa-2x"</a>';
+    container.innerHTML = '<a href="#"><i class="fa fa-plus-square-o fa-2x"</a>';
 
-        container.onclick = function(ev) {
-            ev.preventDefault();
-            map.adding = !map.adding
-                if (map.adding) {
-                    container.querySelector('.fa').classList.add('fa-plus-square');
-                    container.querySelector('.fa').classList.remove('fa-plus-square-o');
-                } else {
-                    container.querySelector('.fa').classList.add('fa-plus-square-o');
-                    container.querySelector('.fa').classList.remove('fa-plus-square');
-                }
-            return false;
-        }
+    container.onclick = function(ev) {
+      ev.preventDefault();
+      map.adding = !map.adding;
+      if (map.adding) {
+        container.querySelector('.fa').classList.add('fa-plus-square');
+        container.querySelector('.fa').classList.remove('fa-plus-square-o');
+      } else {
+        container.querySelector('.fa').classList.add('fa-plus-square-o');
+        container.querySelector('.fa').classList.remove('fa-plus-square');
+      }
+      return false;
+    };
 
-        map.on('click', function(ev) {
-            if (!map.adding || ev.originalEvent.defaultPrevented) return;
-            const modalNode = L.DomUtil.create('div', 'add-modal');
-            modalNode.innerHTML = `
-            <h2>Is this a Fixit-style stand or a Pump?</h2>
-            <form><p>
-            <input id="r-fixit" checked="checked" type="radio" name="style" value="fixit"><label for="r-fixit"><img src="fixit.svg"> Fixit-style stand</label>
-            <input id="r-pump-shop" type="radio" name="style" value="pump+shop"><label for="r-pump-shop"><img src="pump+shop.svg"> Pump + Bike Shop</label>
-            <input id="r-pump" type="radio" name="style" value="pump"><label for="r-pump"><img src="pump.svg"> Pump</label>
-            </p><button type="submit">Submit</button></form>
-            `;
-            L.DomEvent.on(modalNode.querySelector('button[type=submit]'), 'click', function() {
-                modalNode.querySelector('[type=submit]').disabled = "disabled"
-                modalNode.querySelector('[type=submit]').innerHTML = '<i class="fa fa-spinner fa-spin"></i> Submit</i>';
-                fetch(`${API_ROOT}/stand`, {
-                    method: 'POST',
-                    body: JSON.stringify({
-                      stand_type: new FormData(this.form).get('style'),
-                      lat: ev.latlng.lat,
-                      lng: ev.latlng.lng,
-                    }),
-                    cors: true,
-                })
-                    .then(function(resp) {
-                        return resp.json()
-                    })
-                    .then(function(json) {
-                        new L.marker(json.coordinates.reverse(), {
-                            icon: L.icon({
-                                iconUrl: `${json.properties.stand_type}.svg`,
-                                iconSize:     [48, 48],
-                                iconAnchor:   [24, 24]
-                            })
-                        }).addTo(map);
-                        map.closeModal();
-                        container.click();
-                    });
-            })
-            map.openModal({element: modalNode});
-        });
+    map.on('click', function(ev) {
+      if (!map.adding || ev.originalEvent.defaultPrevented) return;
+      const modalNode = L.DomUtil.create('div', 'add-modal');
+      modalNode.innerHTML = `
+      <h2>Is this a Fixit-style stand or a Pump?</h2>
+      <form><p>
+      <input id="r-fixit" checked="checked" type="radio" name="style" value="fixit"><label for="r-fixit"><img src="fixit.svg"> Fixit-style stand</label>
+      <input id="r-pump-shop" type="radio" name="style" value="pump+shop"><label for="r-pump-shop"><img src="pump+shop.svg"> Pump + Bike Shop</label>
+      <input id="r-pump" type="radio" name="style" value="pump"><label for="r-pump"><img src="pump.svg"> Pump</label>
+      </p><button type="submit">Submit</button></form>
+      `;
+      L.DomEvent.on(modalNode.querySelector('button[type=submit]'), 'click', function() {
+        modalNode.querySelector('[type=submit]').disabled = 'disabled';
+        modalNode.querySelector('[type=submit]').innerHTML = '<i class="fa fa-spinner fa-spin"></i> Submit</i>';
+        fetch(`${API_ROOT}/stand`, {
+          method: 'POST',
+          body: JSON.stringify({
+            stand_type: new FormData(this.form).get('style'),
+            lat: ev.latlng.lat,
+            lng: ev.latlng.lng,
+          }),
+          cors: true,
+        })
+          .then(function(resp) {
+            return resp.json();
+          })
+          .then(function(json) {
+            new L.marker(json.coordinates.reverse(), {
+              icon: L.icon({
+                iconUrl: `${json.properties.stand_type}.svg`,
+                iconSize: [48, 48],
+                iconAnchor: [24, 24]
+              })
+            }).addTo(map);
+            map.closeModal();
+            container.click();
+          });
+      });
+      map.openModal({element: modalNode});
+    });
 
-        map.adding = false;
+    map.adding = false;
 
-        return container;
-    }
+    return container;
+  }
 });
 
 const helpModal = function(ev) {
@@ -95,77 +95,77 @@ const helpModal = function(ev) {
   <h3>Legend</h3>
   <p class="legend">
   <img src="fixit.svg"> <a href="https://www.google.com/search?tbm=isch&q=fixit%20stand">Fixit stands</a>
-  <br>
+    <br>
   <img src="pump+shop.svg"> Bike pump outside a bike shop
   <br>
   <img src="pump.svg"> Other pulicly accessible bike pump
   </p>
   <h3>Source</h3>
   <p>BikeHero is open source and on <a href="https://github.com/dschep/bikehero">Github</a>.</p>
-  `;
+    `;
   map.openModal({element: modalNode});
   return false;
-}
+};
 
 const HelpControl = L.Control.extend({
-    options: {
-        position: 'topright'
-    },
+  options: {
+    position: 'topright'
+  },
 
-    onAdd: function (map) {
-        // create the control container with a particular class name
-        const container = L.DomUtil.create('div', 'leaflet-bar add-control');
+  onAdd: function () {
+    // create the control container with a particular class name
+    const container = L.DomUtil.create('div', 'leaflet-bar add-control');
 
-        container.innerHTML = '<a href="#"><i class="fa fa-question fa-2x"</a>';
+    container.innerHTML = '<a href="#"><i class="fa fa-question fa-2x"</a>';
 
-        container.onclick = helpModal;
+    container.onclick = helpModal;
 
-        return container;
-    }
+    return container;
+  }
 });
 
 map.addControl(new HelpControl());
 map.addControl(new AddStandControl());
 if (!document.cookie)
-  helpModal()
+  helpModal();
 
 fetch(`${API_ROOT}/stands`, {cors: true})
-.then(function(resp) {
+  .then(function(resp) {
     return resp.json();
-})
-.then(function(geoJson) {
+  })
+  .then(function(geoJson) {
     L.geoJSON(geoJson, {
-        pointToLayer: function (feature, latlng) {
-            return L.marker(latlng, {
-                icon: L.icon({
-                    iconUrl: `${feature.properties.stand_type}.svg`,
-                    iconSize:     [48, 48],
-                    iconAnchor:   [24, 24]
-                })
-            }).on('click', e => {
-              // create geoJson feature just to get bounds of 2 points
-              const bounds = L.geoJson({
-                type: 'FeatureCollection',
-                'features': [
-                  {
-                    type: 'Point',
-                    coordinates: [userLocation.lng, userLocation.lat],
-                  },
-                  {
-                    type: 'Point',
-                    coordinates: [e.latlng.lng, e.latlng.lat],
-                  },
-                ],
-              }).getBounds();
-              map.fitBounds(bounds);
-            });
-        }
+      pointToLayer: function (feature, latlng) {
+        return L.marker(latlng, {
+          icon: L.icon({
+            iconUrl: `${feature.properties.stand_type}.svg`,
+            iconSize:     [48, 48],
+            iconAnchor:   [24, 24]
+          })
+        }).on('click', e => {
+        // create geoJson feature just to get bounds of 2 points
+          const bounds = L.geoJson({
+            type: 'FeatureCollection',
+            'features': [
+              {
+                type: 'Point',
+                coordinates: [userLocation.lng, userLocation.lat],
+              },
+              {
+                type: 'Point',
+                coordinates: [e.latlng.lng, e.latlng.lat],
+              },
+            ],
+          }).getBounds();
+          map.fitBounds(bounds);
+        });
+      }
 
     }).addTo(map);
-});
+  });
 
 const lc = L.control.locate({
-    locateOptions: { maxZoom: 14 }
+  locateOptions: { maxZoom: 14 }
 }).addTo(map);
 map.on('locationfound', e => {userLocation = e.latlng;});
 lc.start();
